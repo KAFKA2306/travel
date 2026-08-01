33438d0fd8566134b08b99f70713d30d7eea0627
KAFKA <137051370+KAFKA2306@users.noreply.github.com>
2026-08-01 23:42:23 +0900

    Mount Google Maps panel

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import GoogleMapsDock from './GoogleMapsDock'
 import './styles.css'
+import './google-maps.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <GoogleMapsDock />
   </StrictMode>,
 )
