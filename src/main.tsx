61023abee1c2169c5283d436dca35e40ab4a99cc
KAFKA <137051370+KAFKA2306@users.noreply.github.com>
2026-08-02 06:43:20 +0900

    Mount maps from app state

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import GoogleMapsDock from './GoogleMapsDock'
 import './styles.css'
 import './google-maps.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <GoogleMapsDock />
   </StrictMode>,
 )
